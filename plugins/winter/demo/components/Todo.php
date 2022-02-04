88f503c953efa8389a33fa7192910aa586be7c10
GrCOTE7 <GrCOTE7@GMail.com>
2022-02-04 16:53:32 +0100

    2Fix Boostswatch en Local

--- plugins/winter/demo/components/Todo.php
+++ plugins/winter/demo/components/Todo.php
@@ -1,45 +1,50 @@
-<?php namespace Winter\Demo\Components;
+<?php
 
-use Cms\Classes\ComponentBase;
+/*
+ * (c) Boosteur.com - 2022
+ */
+
+namespace Winter\Demo\Components;
+
 use ApplicationException;
+use Cms\Classes\ComponentBase;
 
 class Todo extends ComponentBase
 {
+  public function componentDetails()
+  {
+    return [
+      'name'        => 'Todo List',
+      'description' => 'Implements a simple to-do list.',
+    ];
+  }
 
-    public function componentDetails()
-    {
-        return [
-            'name'        => 'Todo List',
-            'description' => 'Implements a simple to-do list.'
+  public function defineProperties()
+  {
+    return [
+      'max' => [
+        'description'       => 'The most amount of todo items allowed',
+        'title'             => 'Max items',
+        'default'           => 10,
+        'type'              => 'string',
+        'validationPattern' => '^[0-9]+$',
+        'validationMessage' => 'The Max Items value is required and should be integer.',
+      ],
         ];
+  }
+
+  public function onAddItem()
+  {
+    $items = post('items', []);
+
+    if (\count($items) >= $this->property('max')) {
+      throw new ApplicationException(sprintf('Sorry only %s items are allowed.', $this->property('max')));
     }
 
-    public function defineProperties()
-    {
-        return [
-            'max' => [
-                'description'       => 'The most amount of todo items allowed',
-                'title'             => 'Max items',
-                'default'           => 10,
-                'type'              => 'string',
-                'validationPattern' => '^[0-9]+$',
-                'validationMessage' => 'The Max Items value is required and should be integer.'
-            ]
-        ];
+    if (($newItem = post('newItem')) !== '') {
+      $items[] = $newItem;
     }
 
-    public function onAddItem()
-    {
-        $items = post('items', []);
-
-        if (count($items) >= $this->property('max')) {
-            throw new ApplicationException(sprintf('Sorry only %s items are allowed.', $this->property('max')));
-        }
-
-        if (($newItem = post('newItem')) != '') {
-            $items[] = $newItem;
-        }
-
-        $this->page['items'] = $items;
-    }
+    $this->page['items'] = $items;
+  }
 }
