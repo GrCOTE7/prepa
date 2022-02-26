71d6c4b74edbdebd76e06bb585196bf206feed70
GrCOTE7 <GrCOTE7@GMail.com>
2022-02-26 10:37:04 +0100

    Ajax Example Form Validator

--- plugins/grcote7/winterlearning/components/Ajax.php
+++ plugins/grcote7/winterlearning/components/Ajax.php
@@ -8,6 +8,8 @@ namespace GrCote7\WinterLearning\Components;
 
 use Cms\Classes\ComponentBase;
 use Flash;
+use ValidationException;
+use Validator;
 
 class Ajax extends ComponentBase
 {
@@ -28,5 +30,23 @@ class Ajax extends ComponentBase
   {
     $this->page['result'] = input('value1') + input('value2');
     Flash::success('Calcul réalisé !');
+  }
+
+  public function onDoSomething()
+  {
+    $data = post();
+
+    $rules = [
+      'email' => 'required|email',
+      'name'  => 'required',
+    ];
+
+    $validation = Validator::make($data, $rules);
+
+    if ($validation->fails()) {
+      throw new ValidationException($validation);
+    }
+
+    Flash::success('Jobs done!');
   }
 }
