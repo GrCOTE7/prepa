6942354a82ca93ab866e216460e7abe7cdf957e6
GrCOTE7 <GrCOTE7@GMail.com>
2022-02-26 15:42:45 +0100

    DB Single value

--- plugins/grcote7/winterlearning/components/Database.php
+++ plugins/grcote7/winterlearning/components/Database.php
@@ -8,6 +8,7 @@ namespace Grcote7\WinterLearning\Components;
 
 use Cms\Classes\ComponentBase;
 use Db;
+use Winter\Storm\Support\Facades\Schema;
 
 class Database extends ComponentBase
 {
@@ -26,9 +27,17 @@ class Database extends ComponentBase
 
   public function users()
   {
+    Schema::table('users', function ($table) {
+      $table->string('name', 50)->change();
+    });
+
     // return ['MP', 'Lionel'];
     // dd(Db::select('select * from users'));
-    $users = Db::select('select * from users');
+    // $users = Db::select('select * from users');
+    $user = Db::table('users')->where('name', 'MP')->value('email');
+    // dd($users);
+    echo $user;
+    exit;
     foreach ($users as $user) {
       $all[] = $user->name.' ('.$user->email.')';
     }
