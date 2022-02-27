70626fbc0139c01e341fcf42b5fce5a544bb417d
GrCOTE7 <GrCOTE7@GMail.com>
2022-02-27 17:31:10 +0100

    Db Skip() & Take()

--- plugins/grcote7/winterlearning/components/Database.php
+++ plugins/grcote7/winterlearning/components/Database.php
@@ -26,11 +26,38 @@ class Database extends ComponentBase
 
   public function content()
   {
-    return Db::table('users')
-      ->select('id', 'name')
-      ->inRandomOrder()
-      ->first();
+    // return 'ok';
 
-    // print_r($data);
+    $data = Db::table('backend_access_log')
+      ->select('id')
+      ->skip(10)
+      ->take(3)
+      ->get();
+
+    $this->testType($data);
+  }
+
+  public function testType($var)
+  {
+    switch ($var) {
+        case is_numeric($var) || \is_string($var):
+        echo $var;
+
+        break;
+        case \is_array($var):
+            echo 'Arr.: '.implode(', ', $var);
+
+            break;
+            case \is_object($var):
+                print_r((array) $var);
+
+            break;
+        default:
+
+            echo 'Autre';
+
+            break;
+    }
+    echo '<br>';
   }
 }
