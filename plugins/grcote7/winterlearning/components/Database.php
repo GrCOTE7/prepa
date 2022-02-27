07ab7e382a4283c0525921640378548d9c613929
GrCOTE7 <GrCOTE7@GMail.com>
2022-02-27 12:10:42 +0100

    Db Different ways to show a Collection

--- plugins/grcote7/winterlearning/components/Database.php
+++ plugins/grcote7/winterlearning/components/Database.php
@@ -29,9 +29,11 @@ class Database extends ComponentBase
     $users = Db::table('users')->select('id', 'name', 'email')->get();
 
     // dd($users);
+    echo '<u><b>Depuis le code PHP dans le component :</b></u><br>';
     foreach ($users as $user) {
-      //   echo $user->name;
+      echo $user->id.' '.$user->name.' '.$user->email.'<br>';
     }
+    echo '<hr><u><b>Depuis le template du component (default.htm) :</b></u><br>';
 
     return $users;
     // var_dump($users);
