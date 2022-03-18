5cca75e78827a0154c4c3eafa31d53d9e7e70fd3
GrCOTE7 <GrCOTE7@gmail.com>
2022-03-18 16:51:31 +0100

    Fixg SQL Request

--- plugins/grcote7/marriage/components/Guests.php
+++ plugins/grcote7/marriage/components/Guests.php
@@ -7,6 +7,8 @@
 namespace Grcote7\Marriage\Components;
 
 use Cms\Classes\ComponentBase;
+use Grcote7\Marriage\Models\Guest;
+use Illuminate\Support\Facades\DB;
 use Winter\User\Models\User;
 
 class Guests extends ComponentBase
@@ -28,6 +30,20 @@ class Guests extends ComponentBase
   {
     // $user = User::find(3);
     // return $user->guest->mobile;
+    // return $data ?? '<p>$data est vide</p>';
+    // $data = Guest::select('mobile')->where('id', 1)->first();
+
+    $data = Guest::selectConcat(['Id: ', 'id'], 'numId');
+    $data = $data->addSelect('mobile');
+
+    // $data = $data->where('id', 3);
+    DB::connection()->enableQueryLog();
+    $queries = DB::getQueryLog();
+
+    $last_query = end($queries);
+
+    return [$data->get(), $last_query];
+    // dd($data);
     // return $data ?? '<p>$data est vide</p>';
   }
 }
