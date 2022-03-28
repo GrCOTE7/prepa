e47f35b21e2adb04d7e4d7b3f81a2c2e5a32a3b7
GrCOTE7 <GrCOTE7@gmail.com>
2022-03-29 01:27:07 +0200

    Users has ('guest')

--- plugins/grcote7/marriage/components/Guests.php
+++ plugins/grcote7/marriage/components/Guests.php
@@ -7,9 +7,9 @@
 namespace Grcote7\Marriage\Components;
 
 use Cms\Classes\ComponentBase;
-use Grcote7\Marriage\Models\Familly;
 use Grcote7\Marriage\Models\Group;
 use Grcote7\Marriage\Models\Guest;
+use Winter\User\Models\User;
 
 class Guests extends ComponentBase
 {
@@ -33,43 +33,10 @@ class Guests extends ComponentBase
     // $ft  = 'grcote7_marriage_famillies';
     // $ggt = 'grcote7_marriage_group_guest';
 
-    $data[] = 'Guest → familly & Groups';
-    $gs     = Guest::find(1);
-    $data[] = $gs->user->name;
-    $data[] = $gs->mobile;
-    $data[] = 'Famille: '.$gs->familly->name;
-    foreach ($gs->groups as $g) {
-      $data[] = $g->id.' - '.$g->name;
-    }
+    $data[] = 'User → Has guest';
+    $data[] = user::select('name')->has('guest')->get();
 
-    $data[] = str_repeat('-', 45);
-
-    $data[] = 'Familly → guests & groups';
-    $fs     = Familly::find(1);
-    $data[] = 'Famille: '.$fs->name;
-    // dd($fs->guests);
-    foreach ($fs->guests as $f) {
-      $data[] = '- '.$f->user->name;
-      foreach ($f->groups as $gu) {
-        $data[] = '   - '.$gu->name;
-      }
-    }
-
-    $data[] = str_repeat('-', 45);
-
-    $data[] = 'Group → guests & familly';
-    $gr     = Group::find(3);
-    $data[] = 'Group: '.$gr->name;
-    // dd($gr->guests);
-    foreach ($gr->guests as $g) {
-      $data[] = '- '.$g->user->name;
-      foreach ($f->groups as $gu) {
-        $data[] = '   - '.$gu->name;
-      }
-    }
-
-    $data[] = str_repeat('-', 45);
-
+    // $data[] = str_repeat('-', 45);
     return $data ?? '<p>$data est vide</p>';
     // ->dump()
     //   ->first()
