bc36c781b78d370650625e0d4a991da5384ad3cc
GrCOTE7 <GrCOTE7@gmail.com>
2022-04-01 12:04:34 +0200

    Reverse morphTO

--- plugins/grcote7/marriage/components/Guests.php
+++ plugins/grcote7/marriage/components/Guests.php
@@ -7,8 +7,7 @@
 namespace Grcote7\Marriage\Components;
 
 use Cms\Classes\ComponentBase;
-use Grcote7\Marriage\Models\Group;
-use Grcote7\Marriage\Models\Guest;
+use Grcote7\Marriage\Models\Photo;
 
 class Guests extends ComponentBase
 {
@@ -33,25 +32,19 @@ class Guests extends ComponentBase
     $data[] = 'MorphTo relationship sample :';
     $data[] = str_repeat('-', 45);
 
+    // Reverse relationship morphTo
     $id = 2;
-    $g  = Guest::find($id);
-    if ($g) {
-      $data[] = $g->user->name;
-      $data[] = $g->photo->path;
+    $d  = Photo::find($id);
+    if ($d) {
+      $data[] = $d->path;
+      //   $data[] = $d->guest->id;
+      $data[] = $d->imageable->user->name;
     } else {
       $data[] = 'No user with id '.$id;
     }
-
-    $data[] = str_repeat('-', 45);
-
-    $id = 1;
-    $gr = Group::find($id);
-    if ($gr) {
-      $data[] = $gr->name;
-      $data[] = $gr->photo->path;
-    } else {
-      $data[] = 'No group with id '.$id;
-    }
+    // foreach ($d->groups as $k => $v) {
+    //   $data[] = $v->name;
+    // }
 
     return $data ?? '<p>$data est vide</p>';
     //   ->dump()
