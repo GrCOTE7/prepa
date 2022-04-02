25e12c2a421bf93e83d1f531f591a35eb0d1893f
GrCOTE7 <GrCOTE7@gmail.com>
2022-04-02 11:27:31 +0200

    Nett

--- plugins/grcote7/marriage/components/Guests.php
+++ plugins/grcote7/marriage/components/Guests.php
@@ -7,10 +7,8 @@
 namespace Grcote7\Marriage\Components;
 
 use Cms\Classes\ComponentBase;
-use Grcote7\Marriage\Models\Guest;
 use Grcote7\Marriage\Models\Photo;
 use Illuminate\Support\Facades\DB;
-use Winter\Storm\Database\Models\DeferredBinding;
 
 class Guests extends ComponentBase
 {
@@ -30,45 +28,7 @@ class Guests extends ComponentBase
 
   public function onRun()
   {
-    // Deferred binding
-    // $sessionKey = uniqid('session_key', true);
-    // $data[]     = $this->getAllPhotos();
-    // // ----------------------------------------------------------------------------------
-
-    // $newTof                 = new Photo();
-    // $newTof->path           = 'SAM.png';
-    // $newTof->imageable_id   = 7;
-    // $newTof->imageable_type = 'guest';
-    // $data[]                 = $newTof;
-    // $newTof->save();
-
-    // // $newTof = Photo::find(8);
-
-    // $newGuest             = new Guest();
-    // $newGuest->user_id    = 5;
-    // $newGuest->familly_id = 3;
-    // $newGuest->mobile     = 'GSM Sam';
-    // $data[]               = $newGuest;
-
-    // $data[] = str_repeat(' ', 45);
-    // $newGuest->photo()->add($newTof, $sessionKey);
-    // // $newGuest->photo()->add($newTof); // Doesn't work because guest not yet created
-    // $newGuest->save();
-
-    // $data[] = $newGuest;
-    // $data[] = str_repeat('-', 45);
-    // // ----------------------------------------------------------------------------------
-    // $data[] = $this->getAllPhotos();
-
-    // $sessionKey = 'session_key624814772291a0.77586480';
-    // $data[]     = Guest::find(7)->cancelDeferred($sessionKey);
-
-    // $data[] = Guest::find(7)->photo()->withDeferred($sessionKey)->get();
-    // $tof   = Photo::find(8);
-    // $guest = Guest::find(7);
-    // $guest->photo()->remove($tof, $sessionKey); // remove the deffered binding
-
-    DeferredBinding::cleanUp(1); // Older than 1 day - Default: 5 days when admin logs in
+    $data[] = 'Ready.';
 
     return $data ?? '<p>$data est vide</p>';
     //   ->dump()
