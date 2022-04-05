ebcb893298d854b9c21514c88ed67c319d48c6e7
GrCOTE7 <GrCOTE7@gmail.com>
2022-04-05 09:29:36 +0200

    Add Builder plugin

--- plugins/winter/test/models/Person.php
+++ plugins/winter/test/models/Person.php
@@ -1,57 +1,63 @@
-<?php namespace Winter\Test\Models;
+<?php
+
+/*
+ * (c) Boosteur.com - 2022
+ */
+
+namespace Winter\Test\Models;
 
 use Model;
 
 /**
- * Person Model
+ * Person Model.
  */
 class Person extends Model
 {
-    use \Winter\Storm\Database\Traits\Nullable;
-    use \Winter\Storm\Database\Traits\Validation;
+  use \Winter\Storm\Database\Traits\Nullable;
+  use \Winter\Storm\Database\Traits\Validation;
 
-    /**
-     * @var string The database table used by the model.
-     */
-    public $table = 'winter_test_people';
+  /**
+   * @var string the database table used by the model
+   */
+  public $table = 'winter_test_people';
 
-    /**
-     * @var array Guarded fields
-     */
-    protected $guarded = [];
+  /**
+   * @var array Dates
+   */
+  public $dates = ['birth', 'birthdate'];
 
-    /**
-     * @var array Fillable fields
-     */
-    protected $fillable = [];
+  /**
+   * @var array Rules
+   */
+  public $rules = [
+    'name' => 'required',
+  ];
 
-    /**
-     * @var array List of attribute names which are json encoded and decoded from the database.
-     */
-    protected $jsonable = ['hobbies', 'sports'];
-
-    /**
-     * @var array List of attribute names which should be set to null when empty.
-     */
-    protected $nullable = ['is_married'];
-
-    /**
-     * @var array Dates
-     */
-    public $dates = ['birth', 'birthdate'];
-
-    /**
-     * @var array Rules
-     */
-    public $rules = [
-        'name' => 'required',
+  /**
+   * @var array Relations
+   */
+  public $hasOne = [
+    'phone'     => ['Winter\Test\Models\Phone', 'key' => 'person_id', 'scope' => 'isActive'],
+    'alt_phone' => ['Winter\Test\Models\Phone', 'key' => 'person_id'],
     ];
 
-    /**
-     * @var array Relations
-     */
-    public $hasOne = [
-        'phone' => ['Winter\Test\Models\Phone', 'key' => 'person_id', 'scope' => 'isActive'],
-        'alt_phone' => ['Winter\Test\Models\Phone', 'key' => 'person_id']
-    ];
+  /**
+   * @var array Guarded fields
+   */
+  protected $guarded = [];
+
+  /**
+   * @var array Fillable fields
+   */
+  protected $fillable = [];
+
+  /**
+   * @var array list of attribute names which are json encoded and decoded from the database
+   */
+  protected $jsonable = ['hobbies', 'sports'];
+
+  /**
+   * @var array list of attribute names which should be set to null when empty
+   */
+  protected $nullable = ['preferred_name', 'is_married'];
 }
