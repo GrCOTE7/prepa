ac17c4a3f5fd75a0669893ff42a081ed951391d5
GrCOTE7 <GrCOTE7@gmail.com>
2022-04-06 08:04:44 +0200

    Add movies gallery

--- plugins/grcote7/movies/models/Movie.php
+++ plugins/grcote7/movies/models/Movie.php
@@ -1,29 +1,45 @@
-<?php namespace Grcote7\Movies\Models;
+<?php
+
+/*
+ * (c) Boosteur.com - 2022
+ */
+
+namespace Grcote7\Movies\Models;
 
 use Model;
 
 /**
- * Model
+ * Model.
  */
 class Movie extends Model
 {
-    use \Winter\Storm\Database\Traits\Validation;
-    
-    /*
-     * Disable timestamps by default.
-     * Remove this line if timestamps are defined in the database table.
-     */
-    public $timestamps = false;
+  use \Winter\Storm\Database\Traits\Validation;
 
+  /*
+   * Disable timestamps by default.
+   * Remove this line if timestamps are defined in the database table.
+   */
+  public $timestamps = false;
 
-    /**
-     * @var string The database table used by the model.
-     */
-    public $table = 'grcote7_movies_movies';
+  /**
+   * Relations.
+   */
+  public $attachOne = [
+    'poster' => 'System\Models\File',
+  ];
 
-    /**
-     * @var array Validation rules
-     */
-    public $rules = [
-    ];
+  public $attachMany = [
+    'movie_gallery' => 'System\Models\File',
+  ];
+
+  /**
+   * @var string the database table used by the model
+   */
+  public $table = 'grcote7_movies_movies';
+
+  /**
+   * @var array Validation rules
+   */
+  public $rules = [
+  ];
 }
