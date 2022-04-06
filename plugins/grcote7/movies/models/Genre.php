9ae30fc3d981269a24b803fedbc7c68200be687c
GrCOTE7 <GrCOTE7@gmail.com>
2022-04-06 09:28:20 +0200

    Reverse relation M2M

--- plugins/grcote7/movies/models/Genre.php
+++ plugins/grcote7/movies/models/Genre.php
@@ -1,29 +1,42 @@
-<?php namespace Grcote7\Movies\Models;
+<?php
+
+/*
+ * (c) Boosteur.com - 2022
+ */
+
+namespace Grcote7\Movies\Models;
 
 use Model;
 
 /**
- * Model
+ * Model.
  */
 class Genre extends Model
 {
-    use \Winter\Storm\Database\Traits\Validation;
-    
-    /*
-     * Disable timestamps by default.
-     * Remove this line if timestamps are defined in the database table.
-     */
-    public $timestamps = false;
+  use \Winter\Storm\Database\Traits\Validation;
 
+  /*
+   * Disable timestamps by default.
+   * Remove this line if timestamps are defined in the database table.
+   */
+  public $timestamps = false;
 
-    /**
-     * @var string The database table used by the model.
-     */
-    public $table = 'grcote7_movies_genres';
+  /**
+   * @var string the database table used by the model
+   */
+  public $table = 'grcote7_movies_genres';
 
-    /**
-     * @var array Validation rules
-     */
-    public $rules = [
+  /**
+   * @var array Validation rules
+   */
+  public $rules = [
+  ];
+
+  public $belongsToMany = [
+    'movies' => [
+      'Grcote7\Movies\Models\Movie',
+      'table' => 'grcote7_movies_genres_movies',
+      'order' => 'name',
+    ],
     ];
 }
