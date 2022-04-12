98ed6ed556ef35773da257abc40694a804b36bb6
GrCOTE7 <GrCOTE7@gmail.com>
2022-04-12 19:08:28 +0200

    Front end form addimage

--- plugins/grcote7/movies/components/ActorForm.php
+++ plugins/grcote7/movies/components/ActorForm.php
@@ -35,9 +35,10 @@ class ActorForm extends ComponentBase
 
   public function onSave()
   {
-    $actor           = new Actor();
-    $actor->name     = Input::get('name');
-    $actor->lastname = Input::get('lastname');
+    $actor             = new Actor();
+    $actor->name       = Input::get('name');
+    $actor->lastname   = Input::get('lastname');
+    $actor->actorimage = Input::file('actorimage');
 
     $actor->save();
     Flash::success('Actor added !');
