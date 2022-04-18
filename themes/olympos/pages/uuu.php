e541157970d3f81e79c7e867ae6b2abb242929dd
GrCOTE7 <GrCOTE7@gmail.com>
2022-04-18 11:58:13 +0200

    Up core

---
+++ themes/olympos/pages/uuu.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * (c) Boosteur.com - 2022
+ */
+
+function onStart()
+{
+  // Way 1
+  $this['friends_1'] = [
+    ['name'      => 'Pierre',
+      'lastname' => 'DUPONT',
+    ],
+    [
+      'name'     => 'Paul',
+      'lastname' => 'MARTIN',
+    ],
+  ];
+
+  // Way 2
+  $this['friends_2'] = [
+    ['Pierre' => 'DUPONT'],
+    ['Paul' => 'MARTIN'],
+  ];
+}
